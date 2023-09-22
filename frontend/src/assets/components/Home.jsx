af464ac158e95a3727f0107749c6569c6c3ea64a
unknown <you@example.com>
2023-09-22 20:32:29 +0800

    Gjayz Add

--- frontend/src/assets/components/Home.jsx
+++ frontend/src/assets/components/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './home/header'
 import Footer from './Footer'
+import Content from './home/Content'
 
 export default function Home() {
   return (
     <>
     <Header/>
+    <Content />
     <Footer />
+
     </>
   )
 }
---
+++ frontend/src/assets/components/home/Content.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import imgcontent from '../'
+
+export default function Content() {
+  return (
+    <div className='container-content-home m-auto'>
+        <div className='row-content-home'>
+            <div className='item-contet-home float-right'>
+              <img src='../img/pic_content_home' alt="" />
+            </div>
+            <div className='item-contet-home float-left'>
+              The Computer Product
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic itaque, magni ipsa assumenda dolor corporis.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
