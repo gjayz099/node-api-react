548d8c21517fa16ecb95d3f7a9fd225ef2eca691
unknown <you@example.com>
2023-09-29 14:44:34 +0800

    Gjayz Add

--- frontend/src/assets/components/company/Gallery.jsx
+++ frontend/src/assets/components/company/Gallery.jsx
@@ -1,27 +1,18 @@
 import React from 'react'
+import pic_gallery from './index.json'
 
 
 export default function Gallery() {
   return (
     <div className='container_gallery'>
-        <div className='gallery_row'>
-            <div className='item_gallery'>
-
-            </div>
-
-            <div className='item_gallery'></div>
-
-            <div className='item_gallery'></div>
-
-            <div className='item_gallery'></div>
-
-            <div className='item_gallery'></div>
-
-            <div className='item_gallery'></div>
-
-            <div className='item_gallery'></div>
-
-            <div className='item_gallery'></div>
+        <div className='gallery_row grid'>
+            {
+                pic_gallery.picture_gallery.map((item, id) =>
+                <div className='item_gallery' key={id}>
+                    <img src={item.pic_gallery} alt="" />
+                </div>
+                )
+            }
         </div>
     </div>
   )
